0f5c8a6712202613a2fd7c17dcb55452da4552b2
juanpablo9 <juan.souza@dextra-sw.com>
2021-11-23 18:56:08 -0300

    ajustes no layout e criação dos fragments

---
+++ app/src/main/java/com/example/appcattrilha/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appcattrilha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_home)
+    }
+}
