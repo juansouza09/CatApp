18761e012258117244478f0ba7e9ad18f85b0e96
juanpablo9 <juan.souza@dextra-sw.com>
2021-11-29 17:28:09 -0300

    appbar favoritos arrumada

--- app/src/main/java/com/example/appcattrilha/ActionBottomDialogFragment.kt
+++ app/src/main/java/com/example/appcattrilha/ActionBottomDialogFragment.kt
@@ -36,5 +36,4 @@ class ActionBottomDialogFragment : BottomSheetDialogFragment() {
 
         return binding.root
     }
-
 }
--- app/src/main/java/com/example/appcattrilha/FavoritoActivity.kt
+++ app/src/main/java/com/example/appcattrilha/FavoritoActivity.kt
@@ -1,13 +1,22 @@
 package com.example.appcattrilha
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.example.appcattrilha.databinding.TelaFavoritosBinding
+import com.example.appcattrilha.databinding.TelaLoginBinding
+import kotlinx.android.synthetic.main.tela_home.*
 
 class FavoritoActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.tela_favoritos)
+
+        setSupportActionBar(topAppBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
